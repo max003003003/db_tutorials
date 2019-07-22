3280d6cddde5507e222321c55e669c11f52a766b
max vorabhol <max003003003@gmail.com>
2019-07-22 22:11:32 +0700

    Add get page

--- db.c
+++ db.c
@@ -77,13 +77,35 @@ void deserialize_row(void *source, Row *destination) {
   memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
   memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
+void* get_page(Pager* pager, uint32_t page_num){
+    if(page_num > TABLE_MAX_PAGES) {
+        printf("Tried to fetch page number out of bounds. %d > %d\n", page_num,
+                TABLE_MAX_PAGES);
+        exit(EXIT_FAILURE);
+    }
 
+    if(pager->pages[page_num] == NULL){
+        void* page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_length/ PAGE_SIZE;
+        if(pager->file_length % PAGE_SIZE){
+            num_page+=1;
+        }
+
+        if(page_num <=num_pages){
+            lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+            if(bytes_read == -1){
+                printf("Error reading file: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+        }
+        pager->pages[page_num] = page;
+    }
+    return pager->pages[page_num];
+}
 void *row_slot(Table *table, uint32_t row_num) {
   uint32_t page_num = row_num / ROWS_PER_PAGE;
-  void *page = table->pages[page_num];
-  if (page == NULL) {
-    page = table->pages[page_num] = malloc(PAGE_SIZE);
-  }
+  void* page = get_page(table->page, page_num);
   uint32_t row_offset = row_num % ROWS_PER_PAGE;
   uint32_t byte_offset = row_offset * ROW_SIZE;
   return page + byte_offset;
