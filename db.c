4a1f1479bad0628ba3532bdee7ddfbc9306e967f
max vorabhol <max003003003@gmail.com>
2019-07-23 21:18:43 +0700

    Add cursor advance

--- db.c
+++ db.c
@@ -182,10 +182,16 @@ void db_close(Table *table) {
   free(pager);
   free(table);
 }
-
-void *row_slot(Table *table, uint32_t row_num) {
+void cursor_advance(Cursor* cursor){
+  cursor->row_num +=1;
+  if(cursor->row_num >= cursor->table->num_rows){
+      cursor->end_of_table = true;
+  }
+}
+void *cursor_value(Cursor* cursor) {
+  uint32_t row_num = cursor->row_num;
   uint32_t page_num = row_num / ROWS_PER_PAGE;
-  void *page = get_page(table->pager, page_num);
+  void *page = get_page(cursor->table->pager, page_num);
   uint32_t row_offset = row_num % ROWS_PER_PAGE;
   uint32_t byte_offset = row_offset * ROW_SIZE;
   return page + byte_offset;
